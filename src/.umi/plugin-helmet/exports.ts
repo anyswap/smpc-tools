630cf768e6419d74630a005c71b90ed2cb3c9861
d_winter <2624376436@qq.com>
2022-04-28 17:55:21 +0800

    update .gitignore

--- src/.umi/plugin-helmet/exports.ts
+++
@@ -1,3 +0,0 @@
-// @ts-nocheck
-// @ts-ignore
-export { Helmet } from "D:/Works/dcrm/smpc-tools/node_modules/_react-helmet@6.1.0@react-helmet";
--- src/.umi/plugin-initial-state/exports.ts
+++
@@ -1,7 +0,0 @@
-// @ts-nocheck
-
-// @ts-ignore
-import { InitialState as InitialStateType } from "../plugin-initial-statemodelsinitialState";
-
-export type InitialState = InitialStateType;
-export const __PLUGIN_INITIAL_STATE = 1;
