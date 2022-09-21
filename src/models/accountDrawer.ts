939fee419af5bbdd1ae9635878f6a8558efc0db3
yangangang <1071103972@qq.com>
2022-09-21 10:47:26 +0800

    feat:　If Account null, show createAccoutn

--- src/models/accountDrawer.ts
+++ src/models/accountDrawer.ts
@@ -1,10 +1,12 @@
 import { reducer } from "@/utils";
 import { useReducer, useEffect } from "react";
-
+const Account = JSON.parse(localStorage.getItem("Account") || "[]");
 const initialState = {
   drawerVisible: false,
   activeAccount: null,
-  selectedKeys: ["Coins"],
+  selectedKeys: Account.filter((item: any) => item.Status === "Success").length
+    ? ["Coins"]
+    : ["CreateGrounp"],
 };
 
 export default function Index() {
