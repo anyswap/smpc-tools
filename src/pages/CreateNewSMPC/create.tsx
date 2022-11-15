d1a13bf7d04ae4552373c0e6553c7ffae966a2fc
yangangang <1071103972@qq.com>
2022-11-15 18:58:22 +0800

    feat: page CreateNewSMPC UI UE

--- src/pages/CreateNewSMPC/create.tsx
+++ src/pages/CreateNewSMPC/create.tsx
@@ -1,32 +1,39 @@
 import { Form, Select, Row, Col, Input, Button, Steps } from "antd";
-import React, { useReducer } from "react";
+import React, { useReducer, useEffect } from "react";
 import { useActiveWeb3React } from "@/hooks";
 import "./style.less";
+import { DeleteOutlined } from "@ant-design/icons";
+import { nodeListService } from "@/api";
 
-const initialState = {};
+const initialState = { nodeList: [] };
 const Index = () => {
   const { account } = useActiveWeb3React();
   const [state, dispatch] = useReducer(
     (s, a) => ({ ...s, ...a }),
     initialState
   );
+  const { nodeList } = state;
+  const [form] = Form.useForm();
+  console.info("nodeList", nodeList);
+  const getNodeList = async () => {
+    const res = await nodeListService();
+    dispatch({
+      nodeList: res.info,
+    });
+  };
+
+  useEffect(() => {
+    getNodeList();
+    const interval = setInterval(() => {
+      getNodeList();
+    }, 10000);
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <div className="box">
       <br />
-      <Form size="large">
-        <Form.Item name="keytype" label="MpcType" initialValue="EC256K1">
-          <Select
-            options={[
-              {
-                value: "EC256K1",
-              },
-              {
-                value: "ED25519",
-              },
-            ]}
-            style={{ width: "50%" }}
-          />
-        </Form.Item>
+      <Form size="large" name="smpc" form={form}>
         {/* <Form.Item name="b" label="审批模式" initialValue="随机模式">
             <Select
               options={[
@@ -44,23 +51,108 @@ const Index = () => {
             />
           </Form.Item> */}
         <Row>
-          <Col span={16}>Address</Col>
-          <Col span={7} offset={1}>
-            nodeRpc
+          <Col span={15}>
+            Address<span className="red">*</span>
+          </Col>
+          <Col span={6} offset={1}>
+            RPC/Node Name<span className="red">*</span>
           </Col>
         </Row>
-        <Row>
-          <Col span={16}>
+        <Row className="mt5 mb35">
+          <Col span={15}>
             <Input value={account} />
           </Col>
-          <Col span={7} offset={1}>
-            <Input placeholder="nodeRpc" />
+          <Col span={6} offset={1}>
+            <Form.Item
+              name="b"
+              rules={[{ required: true, message: "Missing last name" }]}
+            >
+              <Select
+                mode="tags"
+                optionLabelProp="label"
+                maxTagCount={3}
+                placeholder="RPC/Node Name"
+              >
+                {nodeList.map((r: any) => (
+                  <Select.Option value={r.name} label={r.name}>
+                    <div className="flex_SB">
+                      <span>{r.name}</span>
+                      <span>{`${(
+                        ((r.createNum - r.createFailNum) / r.createNum) *
+                        100
+                      ).toFixed(2)}%`}</span>
+                    </div>
+                  </Select.Option>
+                ))}
+              </Select>
+            </Form.Item>
           </Col>
         </Row>
-        <Button type="link" className="btn_center">
-          {" "}
-          + Add another owner
-        </Button>
+        <Form.List name="accouts">
+          {(fields, { add, remove }) => (
+            <>
+              {fields.map(({ key, name, ...restField }) => (
+                <Row className="mt10" key={key}>
+                  {console.info("restField==========", {
+                    key,
+                    name,
+                    ...restField,
+                  })}
+                  <Col span={15}>
+                    <Form.Item
+                      {...restField}
+                      name={[name, "first"]}
+                      rules={[
+                        { required: true, message: "Missing first name" },
+                      ]}
+                    >
+                      <Input />
+                    </Form.Item>
+                  </Col>
+                  <Col span={6} offset={1}>
+                    <Form.Item
+                      {...restField}
+                      name={[name, "last"]}
+                      rules={[{ required: true, message: "Missing last name" }]}
+                    >
+                      <Select
+                        mode="tags"
+                        optionLabelProp="label"
+                        maxTagCount={3}
+                        placeholder="RPC/Node Name"
+                      >
+                        {nodeList.map((r: any) => (
+                          <Select.Option value={r.name} label={r.name}>
+                            <div className="flex_SB">
+                              <span>{r.name}</span>
+                              <span>{`${(
+                                ((r.createNum - r.createFailNum) /
+                                  r.createNum) *
+                                100
+                              ).toFixed(2)}%`}</span>
+                            </div>
+                          </Select.Option>
+                        ))}
+                      </Select>
+                    </Form.Item>
+                  </Col>
+                  {fields.length > 1 && (
+                    <Col span={2} className="del-prent">
+                      <DeleteOutlined
+                        onClick={() => remove(name)}
+                        className="cursor_pointer"
+                      />
+                    </Col>
+                  )}
+                </Row>
+              ))}
+              <Button type="link" onClick={add} className="btn_center">
+                + Add another owner
+              </Button>
+            </>
+          )}
+        </Form.List>
+
         <div className="flex_SB">
           <span>
             <Form.Item name="ThresHold" initialValue={"2/2"}>
@@ -72,7 +164,13 @@ const Index = () => {
             </Form.Item>
           </span>
           <span>
-            <Button type="primary">Create SMPC</Button>&nbsp;
+            <Button
+              type="primary"
+              onClick={() => console.info(form.getFieldsValue())}
+            >
+              Create SMPC
+            </Button>
+            &nbsp;
             <Button>Reset</Button>
           </span>
         </div>
